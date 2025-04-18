8600878628e7a64b1dafdfdc540abbe23ac81597
aptem1223 <sanstop357@gmail.com>
2025-04-18 12:15:56 +0300

    Add files via upload

---
+++ Avto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    abstract class Avto
+    {
+        public string Model { get; protected set; }
+        public int EnginePower { get; protected set; }
+        public double FuelConsumption { get; protected set; }
+
+        public Avto(string model, int enginePower, double fuelConsumption)
+        {
+            Model = model;
+            EnginePower = enginePower;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public abstract void DisplayInfo();
+    }
+}
